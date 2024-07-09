999fb4fd635e96641be8c474a7e11f97a7f8347a
Chris Moore <chrismoorelmt@gmail.com>
2024-07-08 23:59:44 -0500

    Added footer link and navigation links, data in form now saved to local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,39 @@
 const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
 let mode = 'light';
+const username = document.querySelector('#username');
+const title = document.querySelector('#title');
+const submit = document.querySelector('#submit');
+const input = document.querySelector('#input');
 
-// Listen for a click event on toggle switch
+
 themeSwitcher.addEventListener('click', function () {
-  // If mode is dark, apply light background
   if (mode === 'dark') {
     mode = 'light';
     container.setAttribute('class', 'light');
-  }
-  // If mode is light, apply dark background
-  else {
+  } else {
     mode = 'dark';
     container.setAttribute('class', 'dark');
   }
 });
+
+submit.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (username === '') {
+    alert('Please provide a user name.');
+  } else if (title === '') {
+    alert('Please provide a title.');
+  } else if (input === '') {
+    alert('Please write an entry.');
+  } else {
+
+    const blogPost = {
+      username: username.value.trim(),
+      title: title.value.trim(),
+      input: input.value.trim(),
+    };
+
+    localStorage.setItem('blogPost', JSON.stringify(blogPost));
+    renderMessage();
+  }
+})
